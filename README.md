fe1e42a6375fd13ceca73cddcdeaead15d734f74
Magnus Runesson <M.Runesson@gmail.com>
2016-04-24 09:44:27 +0200

    Initial commit fetching close stops.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Deviation 
+
+Service that takes coordinates and return disruption in Stockholms
+commute service close to the location.
+
+You need your own API key from [Trafiklab](https://trafiklab.se)
+
+## Run
+
+sbt -Dservices.sl.key="YOUR KEY" run
+
+## Author & license
+
+If you have any questions regarding this project contact:
+
+Magnus Runesson <M.Runesson@gmail.com>.
+
+For licensing info see LICENSE file in project's root directory.
---
+++ src/main/scala/DeviationMicroservice.scala
@@ -0,0 +1,118 @@
+import java.io.IOException
+
+import akka.actor.ActorSystem
+import akka.event.{Logging, LoggingAdapter}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.{ActorMaterializer, Materializer}
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import spray.json.DefaultJsonProtocol
+
+case class Coordinate(lat: Double, long: Double)
+
+case class StopLocation(idx: String, name: String, id: String, lat: String, lon: String, dist: String)
+case class LocationList(noNamespaceSchemaLocation: String, StopLocation: Array[StopLocation])
+case class NearestLocation(LocationList: Option[LocationList])
+
+case class Deviation(deviation: String)
+
+trait Protocols extends DefaultJsonProtocol {
+  implicit val deviationFormat = jsonFormat1(Deviation.apply)
+  implicit val stopLocationFormat = jsonFormat6(StopLocation.apply)
+  implicit val locationListFormat = jsonFormat2(LocationList.apply)
+  implicit val nearestLocationFormat = jsonFormat1(NearestLocation.apply)
+}
+
+trait Service extends Protocols {
+  implicit val system: ActorSystem
+  implicit def executor: ExecutionContextExecutor
+  implicit val materializer: Materializer
+
+  def config: Config
+  lazy val key = config.getString("services.sl.key")
+
+  val logger: LoggingAdapter
+
+  def round(x: Double, n: Int) = ((x * math.pow(10,n) + 0.5).floor / math.pow(10,n))
+
+  lazy val closeStopsConnectionFlow: Flow[HttpRequest, HttpResponse, Any] =
+    Http().outgoingConnection(config.getString("services.sl.server"),
+      config.getInt("services.sl.port"))
+
+
+  def closeStopsRequest(request: HttpRequest): Future[HttpResponse] =
+    Source.single(request).via(closeStopsConnectionFlow).runWith(Sink.head)
+
+  def fetchCloseStops(coord: Coordinate): Future[Either[String, NearestLocation]] = {
+    closeStopsRequest(
+      RequestBuilding.Get(
+        s"/api2/nearbystops.json?key=${key}&originCoordLat=${coord.lat}&originCoordLong=${coord.long}"))
+      .flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[NearestLocation].map(Right(_))
+        case BadRequest => Future.successful(Left(s"$coord: incorrect Coordinates"))
+        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+          val error = s"SL nearbystops API request failed with status code ${response.status} and entity $entity"
+          logger.error(error)
+          Future.failed(new IOException(error))
+        }
+      }
+    }
+  }
+
+
+  def argumentToCoordinate(s: String): Either[String, Coordinate] = {
+    s.split(",") match {
+      case Array(f1, f2) => Right(Coordinate.tupled((round(f1.toDouble,4), round(f2.toDouble, 4))))
+      case _  => Left(s"Incorrect coordinate $s")
+    }
+  }
+
+  def fetchDeviation(coord: Coordinate): ToResponseMarshallable = {
+    fetchCloseStops(coord).map[ToResponseMarshallable] {
+      case Right(nearestLocation) => {
+        nearestLocation.LocationList match {
+          case Some(locaionList) => locaionList.StopLocation
+          case None => BadRequest -> "SL cannot HTTP codes."
+        }
+      }
+      case Left(errorMessage) => BadRequest -> errorMessage
+    }
+  }
+
+  val routes = {
+    logRequestResult("deviation-microservice") {
+      pathPrefix("deviation") {
+        (get & path(Segment)) { arg =>
+          complete {
+            argumentToCoordinate(arg).fold(
+              error => BadRequest -> error,
+              success => fetchDeviation(success)
+            )
+          }
+        }
+      }
+    }
+  }
+}
+
+object DeviationMicroservice extends App with Service {
+  override implicit val system = ActorSystem()
+  override implicit val executor = system.dispatcher
+  override implicit val materializer = ActorMaterializer()
+
+  override val config = ConfigFactory.load()
+  override val logger = Logging(system, getClass)
+
+  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+}
