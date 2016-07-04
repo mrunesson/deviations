d81e1e237675f0135f8d81157159572d45d96056
Magnus Runesson <M.Runesson@gmail.com>
2016-07-04 20:00:44 +0200

    Always publish service on port 8080.

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [ -z ${DEVIATIONS} ] && echo Set DEVIATIONS environment variable to your deviation API key && exit 1
 [ -z ${NEARBYSTOPS} ] && echo Set NEARBYSTOPS environment variable to your near by stops API key && exit 1
-docker run -e DEVIATIONS -e NEARBYSTOPS -P deviations
+docker run -e DEVIATIONS -e NEARBYSTOPS -p 8080:8080 deviations
